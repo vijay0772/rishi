879c098418bc033da6b97f3ff3597fe8a7a14e4a
Vijay Bharvad <vijaybharvad@Vijays-MacBook-Pro.local>
2024-02-01 00:34:49 -0600

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,143 @@
+const logotext = "Rishii:)";
+const meta = {
+    title: "Rishi Patel",
+    description: "I’m Rishi Patel,Web developer, Data Analyst,currently working in Chicago",
+};
+
+const introdata = {
+    title: "I’m Rishi Patel",
+    animated: {
+        first: "Web developer",
+        second: "Data Analyst",
+    },
+    description: "",
+    your_img_url: "https://i.ibb.co/wSGx8NX/resized-person-image.png",
+    resume_url: 'https://i.ibb.co/r3hJtfY/Rishi-Sanjaykumar-Patel-CV-3-copy.jpg',
+
+};
+const dataabout = {
+    title: "About myself",
+    aboutme: "As a proactive and dedicated Master's student in Computer Science at Indiana University's Luddy School of Informatics, Computing, and Engineering, I have a solid foundation in both theoretical and practical aspects of software development. My skill set includes proficiency in Python, PHP, C, and C++, enhanced by familiarity with frameworks like Flutter, Laravel, and Flask. Through intern roles, I have made significant contributions to various web and app development projects, with a particular focus on machine learning and backend technologies. I am passionate about utilizing technology to create efficient solutions, aiming to improve operational efficiency and user experiences in diverse projects.",
+
+};
+const worktimeline = [{
+        jobtitle: "E-Commerce Web Developer Intern",
+        where: "Helicore Info Pvt.Ltd",
+        date: "2021",
+    },
+    {
+        jobtitle: "Web Development Intern",
+        where: "Infikey Technologies Pvt.Ltd",
+        date: "2021",
+    },
+    {
+        jobtitle: "AI/ML Software Developer Intern",
+        where: "Axisray Pvt.Ltd",
+        date: "2023",
+    },
+    {
+        jobtitle: "Data Analyst Intern ",
+        where: "Saint Louis University(Excelerate)",
+        date: "2023",
+    },
+];
+
+const skills = [{
+        name: "HTML",
+        value: 95,
+    },
+    {
+        name: "CSS",
+        value: 85,
+    },
+    {
+        name: "Java",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+    {
+        name: "Mongodb",
+        value: 85,
+    },
+    {
+        name: "MySql",
+        value: 85,
+    },
+    {
+        name: "Nodejs",
+        value: 85,
+    },
+    {
+        name: "Php",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Data Analyst Intern",
+        description: "  ",
+    },
+    {
+        title: "Full-Stack Developer Intern",
+        description: "",
+    },
+    {
+        title: "Software Engineer Intern Intern",
+        description: "",
+    },
+   
+];
+
+const dataportfolio = [{
+        img: "https://i.ibb.co/QCMrcfH/aquaintelli-resized.png",
+        description: "Created a machine learning model for irrigation prediction and an IoT system for water management in agriculture.",
+        name: "AquaIntelli",
+        link: "#",
+    },
+    {
+        img: "https://i.ibb.co/jTLQB39/pawsconnect-resized.png",
+        description: "Developed a pet care app using Flutter/Dart, featuring adoption and veterinary services, with Firebase integration.",
+        name: "PawsConnect ",
+
+        link: "#",
+    },
+    {
+        img: "https://i.ibb.co/g65PKrX/vitalfit-clubhub-resized.png",
+        description: "Developed a fitness club website with HTML, CSS, JavaScript, PHP, and MySQL, streamlining operations and enhancing user collaboration.",
+        name: "VitalFit ClubHub",
+
+        link: "#",
+    },
+        
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "rsp3@iu.edu",
+    YOUR_FONE: "+1(331)245-4491",
+    description: "Hello there! Thanks for taking the time to explore my portfolio. If you have any questions, are interested in collaborating, or would like to discuss potential projects, I'd love to hear from you ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_iglx0nq",
+    YOUR_TEMPLATE_ID: "template_livde7r",
+    YOUR_USER_ID: "shQaHD-5S_29UtCI7",
+};
+
+const socialprofils = {
+    github: "https://github.com/patelrishi2308",
+    linkedin: "https://www.linkedin.com/in/patelrishi23/",
+};
+export {
+    contactConfig, dataabout,
+    dataportfolio, introdata, logotext, meta, services, skills, socialprofils, worktimeline
+};
